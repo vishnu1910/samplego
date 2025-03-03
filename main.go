1204105c95a0e38846294204307994445728f86f
vishnu1910 <62199080+vishnu1910@users.noreply.github.com>
2025-03-03 23:48:30 +0530

    basic files

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"./gen"
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	person := &personpb.Person{
+		Name: "John Doe",
+		Age: 30,
+	}
+
+	data, err := proto.Marshall(person)
+	if err != nil {
+		log.Fatalf("Failed to encode person: %v", err)
+	}
+
+	fmt.Println("Serialized Data:", data)
+
+	var newPerson personpb.Person
+	err = proto.Unmarshal(data, &newPerson)
+	if err != nil {
+		log.Fatalf("Failed to decode person: %v", err)
+	}
+
+	fmt.Println("Deserialized Person:", newPerson)
+}
